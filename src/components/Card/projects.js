7b18480c523186ce35742c05b1f5b20f60f6628d
Abdullah <abdullahh93@gmail.com>
2023-10-23 00:41:23 +0200

    rework

---
+++ src/components/Card/projects.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: 'TDC - Telecommunication',
+    description:
+      'Deliver high-quality, modern production code for a diverse customer needs, with ... Lorem ipsum dolor si amet. Lorem ipsum dolor si amet.. Lorem ipsum dolor si amet..',
+    tags: ['React', 'Typescript', 'Gatsby', 'GraphQL', 'Jest', 'Contentful']
+  },
+  {
+    title: 'Minutscript',
+    description:
+      'Deliver high-quality, modern production code for a diverse customer needs, with ... Lorem ipsum do',
+    tags: ['React', 'Typescript']
+  },
+  {
+    title: 'Massweb - fictional company',
+    description:
+      'Deliver high-quality, modern production code for a diverse customer needs, with ... Lorem ipsum dolor si amet. Lorem ipsum dolor si amet.. Lorem ipsum dolor si amet..',
+    tags: ['React', 'Typescript']
+  },
+  {
+    title: 'PHP webshop',
+    description:
+      'Deliver high-quality, modern production code for a diverse customer needs, with ... Lore',
+    tags: ['PHP', 'MySQL']
+  },
+  {
+    title: 'Test project name',
+    description:
+      'Deliver high-quality, modern production code for a diverse customer needs, with ... Lore',
+    tags: ['This', 'Is', 'A', 'Dummy']
+  }
+]
