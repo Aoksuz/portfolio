fc789e02dab3ef1354dec8b06af53bbd6cf8275d
Abdullah <abdullahh93@gmail.com>
2023-10-27 00:20:37 +0200

    added dark mode v1

--- src/components/Card/projects.js
+++ src/components/Card/projects.js
@@ -3,30 +3,35 @@ export const projects = [
     title: 'TDC - Telecommunication',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lorem ipsum dolor si amet. Lorem ipsum dolor si amet.. Lorem ipsum dolor si amet..',
-    tags: ['React', 'Typescript', 'Gatsby', 'GraphQL', 'Jest', 'Contentful']
+    tags: ['React', 'Typescript', 'Gatsby', 'GraphQL', 'Jest', 'Contentful'],
+    href: 'https://tdc.dk'
   },
   {
     title: 'Minutscript',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lorem ipsum do',
-    tags: ['React', 'Typescript']
+    tags: ['React', 'Typescript'],
+    href: 'https://minutscript.vercel.app/'
   },
   {
     title: 'Massweb - fictional company',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lorem ipsum dolor si amet. Lorem ipsum dolor si amet.. Lorem ipsum dolor si amet..',
-    tags: ['React', 'Typescript']
+    tags: ['React', 'Typescript'],
+    href: '#'
   },
   {
     title: 'PHP webshop',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lore',
-    tags: ['PHP', 'MySQL']
+    tags: ['PHP', 'MySQL'],
+    href: '#'
   },
   {
     title: 'Test project name',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lore',
-    tags: ['This', 'Is', 'A', 'Dummy']
+    tags: ['This', 'Is', 'A', 'Dummy'],
+    href: '#'
   }
 ]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     screens: {
       sm: '480px',
@@ -19,7 +20,10 @@ module.exports = {
         light: '#E5E6E0',
         DEFAULT: '#727272'
       },
-      white: '#FAFAFA'
+      yellow: '#FFAA00',
+      white: '#FAFAFA',
+      transparent: 'transparent',
+      current: 'currentColor'
     },
     fontFamily: {
       title: ['Saira', 'sans-serif'],
