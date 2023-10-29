907a31394415ec99a35da286f97ffa1e8d2fa544
Abdullah <abdullahh93@gmail.com>
2023-10-29 23:26:37 +0100

    updated projects

--- src/components/Card/projects.js
+++ src/components/Card/projects.js
@@ -10,28 +10,35 @@ export const projects = [
     title: 'Minutscript',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lorem ipsum do',
-    tags: ['React', 'Typescript'],
+    tags: ['React', 'Typescript', 'Gatsby', 'UI/UX', 'Figma'],
     href: 'https://minutscript.vercel.app/'
   },
   {
     title: 'Massweb - fictional company',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lorem ipsum dolor si amet. Lorem ipsum dolor si amet.. Lorem ipsum dolor si amet..',
-    tags: ['React', 'Typescript'],
-    href: '#'
+    tags: ['HTML', 'Sass', 'SEO', 'jQuery', 'Figma', 'UI/UX'],
+    href: 'https://dev.oksuz.dk/massweb/'
   },
   {
     title: 'PHP webshop',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lore',
-    tags: ['PHP', 'MySQL'],
-    href: '#'
+    tags: ['PHP', 'MySQL', 'JavaScript', 'Ajax', 'Bootstrap', 'Full Stack'],
+    href: 'https://dev.oksuz.dk/php-webshop/'
   },
   {
-    title: 'Test project name',
+    title: 'Email Interface',
     description:
       'Deliver high-quality, modern production code for a diverse customer needs, with ... Lore',
-    tags: ['This', 'Is', 'A', 'Dummy'],
-    href: '#'
+    tags: ['HTML', 'JavaScript', 'Sass'],
+    href: 'https://dev.oksuz.dk/email-interface/'
+  },
+  {
+    title: 'Gulp Demonstration',
+    description:
+      'Deliver high-quality, modern production code for a diverse customer needs, with ... Lore',
+    tags: ['Gulp', 'HTML', 'Sass', 'BrowserSync'],
+    href: 'https://dev.oksuz.dk/gulp/'
   }
 ]
