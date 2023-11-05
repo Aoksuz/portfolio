3122d9f860d5b38c45a7990a7ba5cd507f4eeacd
Abdullah <abdullahh93@gmail.com>
2023-11-05 13:15:56 +0100

    updated fac and project text

---
+++ src/components/Projects/projects.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: 'TDC - Telecommunication',
+    description:
+      'Crafting high-quality, adaptable code for diverse user needs, optimizing website performance, integrating diverse technologies for enhanced functionality, and collaborating on intuitive web interfaces with UX/UI teams.',
+    tags: ['React', 'Typescript', 'Gatsby', 'GraphQL', 'Jest', 'Contentful'],
+    href: 'https://tdc.dk'
+  },
+  {
+    title: 'Minutscript',
+    description:
+      'Developed comprehensive web solutions using cutting-edge technologies tailored for a startup SaaS company. Designed with a user-centric approach, focusing on the entire user journey to ensure a seamless and intuitive experience.',
+    tags: ['React', 'Typescript', 'Gatsby', 'UI/UX', 'Figma'],
+    href: 'https://minutscript.vercel.app/'
+  },
+  {
+    title: 'Massweb',
+    description:
+      'Created a comprehensive project for a fictional company, emphasizing holistic design, user experience, and SEO strategies to enhance overall performance and user engagement.',
+    tags: ['HTML', 'Sass', 'SEO', 'jQuery', 'Figma', 'UI/UX'],
+    href: 'https://dev.oksuz.dk/massweb/'
+  },
+  {
+    title: 'PHP webshop',
+    description:
+      'Developed a PHP full-stack webshop with user and admin logins for access to distinct panels. Prioritized functionality to enhance user experience.',
+    tags: ['PHP', 'MySQL', 'JavaScript', 'Ajax', 'Bootstrap', 'Full Stack'],
+    href: 'https://dev.oksuz.dk/php-webshop/'
+  },
+  {
+    title: 'Email Interface',
+    description:
+      'Developed streamlined email interfaces emphasizing simplicity by eliminating unnecessary or rarely used functionalities found in contemporary email services.',
+    tags: ['HTML', 'JavaScript', 'Sass'],
+    href: 'https://dev.oksuz.dk/email-interface/'
+  },
+  {
+    title: 'Gulp',
+    description:
+      'Presented a simplified web demonstration showcasing Gulp and its services in a straightforward structure.',
+    tags: ['Gulp', 'HTML', 'Sass', 'BrowserSync'],
+    href: 'https://dev.oksuz.dk/gulp/'
+  }
+]
