cc921e27eaa020de8a8b06ec3215958490052ba9
Abdullah <abdullahh93@gmail.com>
2025-03-04 22:31:31 +0100

    added project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import { execSync } from 'node:child_process'
 
 export default defineConfig({
-  integrations: [tailwind()],
-  base: '/dev/tv-ratings/',
-  hooks: {
-    'astro:build:start': async () => {
-      console.log('Building Vue app...')
-      execSync('cd dev/tv-ratings && npm install && npm run build', {
-        stdio: 'inherit'
-      })
-      console.log('Vue app built successfully!')
-    }
-  }
+  integrations: [tailwind()]
 })
