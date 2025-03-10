c8e15b32db6aa531271e94e1cc0c35bd08ece7f6
Abdullah <abdullahh93@gmail.com>
2025-03-10 20:14:37 +0100

    updated experiences

---
+++ src/components/Experiences/experiences.js
@@ -0,0 +1,12 @@
+export const experiences = [
+  {
+    company: 'Capgemini',
+    preiod: 'Feb 2023 - present',
+    role: 'Frontend developer'
+  },
+  {
+    company: 'Nuuday',
+    preiod: 'Aug 2021 - Feb 2023',
+    role: 'Frontend developer'
+  }
+]
